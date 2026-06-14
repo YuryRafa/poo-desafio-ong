c8f1e5b39957aac539330355be22f5a5651cae85
Yury Rafael <badpiggiesyury@gmail.com>
2026-06-13 23:52:51 -0300

    feat: Adding ProjetoSocial class and project start method

--- ProjetoSocial.java
+++ ProjetoSocial.java
@@ -1,1 +1,18 @@
-public class ProjetoSocial
+public class ProjetoSocial {
+    public String nomeDoProjeto;
+    public int metaImpacto;
+    public Voluntario lider;
+
+    public ProjetoSocial(String nomeDoProjeto, int metaImpacto, Voluntario lider) {
+        this.nomeDoProjeto = nomeDoProjeto;
+        this.metaImpacto = metaImpacto;
+        this.lider = lider;
+    }
+
+    public string iniciarProjeto() {
+        return "Projeto " + nomeDoProjeto + " iniciado, liderado por " + lider.getNome() + ". Meta de impacto: " + metaImpacto;
+    }
+
+
+    
+}
